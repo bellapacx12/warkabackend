dadcc9cfd62368b81e07f3c4ad94b1ed5e2fabc7
bellapacx <bellapacx@gmail.com>
2026-04-18 15:22:34 +0300

    sjsjsssdsdsdsdjjggsddsd

--- config/db.go
+++ config/db.go
@@ -10,7 +10,7 @@ import (
 var DB *sql.DB
 
 func ConnectDB() {
-	connStr := "postgres://user:password@localhost:5432/bingo?sslmode=disable"
+	connStr := "postgresql://neondb_owner:npg_5No6cWapByir@ep-dark-mountain-am0ai3u0-pooler.c-5.us-east-1.aws.neon.tech/neondb?sslmode=require&channel_binding=require"
 
 	db, err := sql.Open("postgres", connStr)
 	if err != nil {
